80e96a7ee7249e3740befecff52eca91d0e72c0f
刘泉 <liuq@uc888.cn>
2017-02-26 01:36:02 +0800

    移植了slack的plugin对于pipeline的支持代码

---
+++ src/test/java/jenkins/plugins/bearychat/workflow/BearychatSendStepIntegrationTest.java
@@ -0,0 +1,50 @@
+package jenkins.plugins.bearychat.workflow;
+
+
+import hudson.model.Result;
+import jenkins.plugins.bearychat.Messages;
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.jenkinsci.plugins.workflow.steps.StepConfigTester;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+public class BearychatSendStepIntegrationTest {
+    @Rule
+    public JenkinsRule jenkinsRule = new JenkinsRule();
+
+    @Test
+    public void configRoundTrip() throws Exception {
+        BearychatSendStep step1 = new BearychatSendStep("message");
+        step1.setColor("good");
+        step1.setChannel("#channel");
+        step1.setToken("token");
+        step1.setTeamDomain("teamDomain");
+        step1.setFailOnError(true);
+
+        BearychatSendStep step2 = new StepConfigTester(jenkinsRule).configRoundTrip(step1);
+        jenkinsRule.assertEqualDataBoundBeans(step1, step2);
+    }
+
+    @Test
+    public void test_global_config_override() throws Exception {
+        WorkflowJob job = jenkinsRule.jenkins.createProject(WorkflowJob.class, "workflow");
+        //just define message
+        job.setDefinition(new CpsFlowDefinition("bearychatSend(message: 'message', teamDomain: 'teamDomain', token: 'token', channel: '#channel', color: 'good');", true));
+        WorkflowRun run = jenkinsRule.assertBuildStatusSuccess(job.scheduleBuild2(0).get());
+        //everything should come from step configuration
+        jenkinsRule.assertLogContains(Messages.BearychatSendStepConfig(false, false, false, false), run);
+    }
+
+    @Test
+    public void test_fail_on_error() throws Exception {
+        WorkflowJob job = jenkinsRule.jenkins.createProject(WorkflowJob.class, "workflow");
+        //just define message
+        job.setDefinition(new CpsFlowDefinition("bearychatSend(message: 'message', teamDomain: 'teamDomain', token: 'token', channel: '#channel', color: 'good', failOnError: true);", true));
+        WorkflowRun run = jenkinsRule.assertBuildStatus(Result.FAILURE, job.scheduleBuild2(0).get());
+        //everything should come from step configuration
+        jenkinsRule.assertLogContains(Messages.NotificationFailed(), run);
+    }
+}
